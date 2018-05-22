0777d78a4d91d187395ee0ae8c2ddceac903e9bf
Rishabh Sharma <rsharma@qca-sdc.com>
2018-05-22 10:21:46 -0500

    Add RequestHeaderController testCase Passed

--- src/main/java/com/rishabh/HeaderParser/service/RequestHeaderService.java
+++ src/main/java/com/rishabh/HeaderParser/service/RequestHeaderService.java
@@ -2,9 +2,11 @@ package com.rishabh.HeaderParser.service;
 
 import com.rishabh.HeaderParser.domain.UserRequestHeader;
 
+import javax.servlet.http.HttpServletRequest;
+
 public class RequestHeaderService {
 
-    public UserRequestHeader getRequestHeader(Object any) {
+    public UserRequestHeader getRequestHeader(HttpServletRequest httpServletRequest) {
         return null;
     }
 }
--- src/main/java/com/rishabh/HeaderParser/web/RequestHeaderController.java
+++ src/main/java/com/rishabh/HeaderParser/web/RequestHeaderController.java
@@ -1,7 +1,32 @@
 package com.rishabh.HeaderParser.web;
 
+import com.rishabh.HeaderParser.domain.UserRequestHeader;
+import com.rishabh.HeaderParser.service.RequestHeaderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class RequestHeaderController {
+
+    @Autowired
+    RequestHeaderService requestHeaderService;
+
+    @RequestMapping(value = "get/userRequestHeader", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public ResponseEntity<UserRequestHeader> getUserRequestHeader(HttpServletRequest httpServletRequest){
+       try{
+           UserRequestHeader userRequestHeader = requestHeaderService.getRequestHeader(httpServletRequest);
+           return new ResponseEntity<>(userRequestHeader,HttpStatus.OK);
+       }
+       catch (Exception e)
+       {
+           return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+       }
+    }
 }
